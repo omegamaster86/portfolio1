f3d0b64285988956bea1100cfb1bac2029fdf79b
omegamaster86 <masya868686@gmail.com>
2023-12-03 18:53:58 +0900

    first commit

--- front/.next/static/chunks/webpack.js
+++ front/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "305d2a99ddd83acb"; }
+/******/ 		__webpack_require__.h = function() { return "7823072c7f2bc1b4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1216,11 +1216,6 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
-/******/ 	}();
-/******/ 	
-/******/ 	/* webpack/runtime/nonce */
-/******/ 	!function() {
-/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,6 +1,41 @@
-import '/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
+// import React from 'react';
+// import { useSession, signIn, signOut } from 'next-auth/react';
+// import { NextPage } from 'next';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// export const Login: NextPage = () => {
+//   const { data: session } = useSession();
+//   return (
+//       <>
+//           {
+//               session && (
+//                   <div>
+//                      <h1>ようこそ, {session.user && session.user.name}さん</h1>
+//                       <button onClick={() => signOut()}>ログアウト</button>
+//                   </div>
+//               )
+//           }
+//           {
+//               !session && (
+//                   <div>
+//                       <p>ログインしていません</p>
+//                       <button onClick={() => signIn()}>ログイン</button>
+//                   </div>
+//               )
+//           }
+//       </>
+//   );
+// };
+
+
+export default function App({ 
+	Component, 
+	pageProps: { session, ...pageProps }
+	}: AppProps) {
+    return (
+    <SessionProvider session={session}>
+	    <Component {...pageProps} />
+    </SessionProvider>
+    );
 }
