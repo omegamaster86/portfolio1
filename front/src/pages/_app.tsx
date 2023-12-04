8905a0728b7cb01e4e0d34d7202b615d3ab9b04a
omegamaster86 <masya868686@gmail.com>
2023-12-04 22:15:30 +0900

    add : SessionProvider

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,41 +1,14 @@
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import React from 'react';
-// import { useSession, signIn, signOut } from 'next-auth/react';
-// import { NextPage } from 'next';
-
-// export const Login: NextPage = () => {
-//   const { data: session } = useSession();
-//   return (
-//       <>
-//           {
-//               session && (
-//                   <div>
-//                      <h1>ようこそ, {session.user && session.user.name}さん</h1>
-//                       <button onClick={() => signOut()}>ログアウト</button>
-//                   </div>
-//               )
-//           }
-//           {
-//               !session && (
-//                   <div>
-//                       <p>ログインしていません</p>
-//                       <button onClick={() => signIn()}>ログイン</button>
-//                   </div>
-//               )
-//           }
-//       </>
-//   );
-// };
-
 
 export default function App({ 
 	Component, 
 	pageProps: { session, ...pageProps }
 	}: AppProps) {
     return (
-    <SessionProvider session={session}>
-	    <Component {...pageProps} />
-    </SessionProvider>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     );
 }
