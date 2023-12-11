41aa4327f368d2703e524cb60698c5799347d032
omegamaster86 <masya868686@gmail.com>
2023-12-11 21:52:26 +0900

    delete : comment

--- front/pages/api/auth/[...nextauth].js
+++ front/pages/api/auth/[...nextauth].js
@@ -8,7 +8,6 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    // ...add more providers here
   ],
 }
 
