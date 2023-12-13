3a0248a0b69669540304a0ec410e1794fd7bfbb2
omegamaster86 <masya868686@gmail.com>
2023-12-13 22:13:33 +0900

    add : secret: process.env.

--- front/pages/api/auth/[...nextauth].js
+++ front/pages/api/auth/[...nextauth].js
@@ -9,6 +9,21 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
+//   callbacks: {
+//     async jwt({ token, account }) {
+//       // Persist the OAuth access_token to the token right after signin
+//       if (account) {
+//         token.accessToken = account.access_token
+//       }
+//       return token
+//     },
+//     async session({ session, token, user }) {
+//       // Send properties to the client, like an access_token from a provider.
+//       session.accessToken = token.accessToken
+//       return session
+//     }
+//   }
 }
 
 export default NextAuth(authOptions)
