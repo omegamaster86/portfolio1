e47651190f8118cd76585030f81c004ff85e152d
omegamaster86 <masya868686@gmail.com>
2023-12-17 19:33:38 +0900

    delete : comment

--- front/pages/api/auth/[...nextauth].js
+++ front/pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
-export const authOptions = {
+export default NextAuth ({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
@@ -9,21 +9,4 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
-//   callbacks: {
-//     async jwt({ token, account }) {
-//       // Persist the OAuth access_token to the token right after signin
-//       if (account) {
-//         token.accessToken = account.access_token
-//       }
-//       return token
-//     },
-//     async session({ session, token, user }) {
-//       // Send properties to the client, like an access_token from a provider.
-//       session.accessToken = token.accessToken
-//       return session
-//     }
-//   }
-}
-
-export default NextAuth(authOptions)
+})
