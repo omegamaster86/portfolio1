a2b07fd837742cbdfa0b67c09b69094e690a5972
omegamaster86 <masya868686@gmail.com>
2023-12-19 22:27:49 +0900

    comment out 59~

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -56,19 +56,19 @@ export const User = ({session, handleSignOut}) => {
   )
 }
 // この下をコメントアウトするとグーグルログインが可能
-export async function getServerSideProps({ req }) {
-  const session = await getSession({ req }) 
+// export async function getServerSideProps({ req }) {
+//   const session = await getSession({ req }) 
 
-  if(!session){
-    return{
-      redirect: {
-        destination:'/login',
-        permanent:false
-      }
-    }
-  }
+//   if(!session){
+//     return{
+//       redirect: {
+//         destination:'/login',
+//         permanent:false
+//       }
+//     }
+//   }
 
-  return{
-    props:{session}
-  }
-}
+//   return{
+//     props:{session}
+//   }
+// }
