cf0e2808f676920b76ac9b9ca984e54e915c2d48
omegamaster86 <masya868686@gmail.com>
2023-12-19 23:07:53 +0900

    post toの受け取りを変更

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users
-  post 'auth/:provider/callback', to: 'api/v1/users#create'
+  post 'auth/:provider/callback', to: '/users#create'
   delete 'users/:email', to: 'api/v1/users#destroy', constraints: { email: %r{[^/]+} }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
