96e37122c15dac63919ed716735d1b789335f8f7
omegamaster86 <masya868686@gmail.com>
2023-12-21 00:46:38 +0900

    callback以下をコメントアウト

--- front/pages/api/auth/[...nextauth].js
+++ front/pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import axios from 'axios'
 
-
+const apiUrl = process.env.NEXT_PUBLIC_API_URL
 
 export default NextAuth ({
   // Configure one or more authentication providers
@@ -12,31 +12,29 @@ export default NextAuth ({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  callbacks: {
-    async signIn({ user, account }) {
-      const apiUrl = process.env.NEXT_PUBLIC_API_URL
-      const provider = account.provider
-      const uid = account.sub
-      const name = user.name
-      const email = user.email
+  // callbacks: {
+  //   async signIn({ user, account }) {
+  //     const provider = account.provider
+  //     const uid = account.sub
+  //     const name = user.name
+  //     const email = user.email
 
-      try {
-        const response = await axios.post(`${apiUrl}/auth/${provider}/callback`, {
-          provider,
-          uid,
-          name,
-          email,
-        })
-
-        if (response.status === 200) {
-          return true
-        } else {
-          return false
-        }
-      } catch (error) {
-        console.log('エラー', error)
-        return false
-      }
-    },
-  },
+  //     try {
+  //       const response = await axios.post(`${apiUrl}/auth/${provider}/callback`, {
+  //         provider,
+  //         uid,
+  //         name,
+  //         email,
+  //       })
+  //       if (response.status === 200) {
+  //         return true
+  //       } else {
+  //         return false
+  //       }
+  //     } catch (error) {
+  //       console.log('エラー', error)
+  //       return false
+  //     }
+  //   },
+  // },
 })
