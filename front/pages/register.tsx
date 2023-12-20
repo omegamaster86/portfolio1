ad62bf0d9e53c1487fb9dbb935ae9d9692a0929c
omegamaster86 <masya868686@gmail.com>
2023-12-20 22:28:21 +0900

    コメントアウトを削除

--- front/pages/register.tsx
+++ front/pages/register.tsx
@@ -18,38 +18,6 @@ export default function Register(){
                 </div>
                 <EmailPassword/>
                 <form className=" flex flex-col gap-5">
-                    {/* <div className="flex border rounded-xl relative">
-                        <input 
-                        className="w-full py-4 px-6 border rounded-xl bg-slate-50 focus:outline-none border-none"
-                        type="text"
-                        name="Username"
-                        placeholder="Username"
-                        />
-                    </div>
-                    <div className="flex border rounded-xl relative">
-                        <input 
-                        className="w-full py-4 px-6 border rounded-xl bg-slate-50 focus:outline-none border-none"
-                        type="email"
-                        name="email"
-                        placeholder="Email"
-                        />
-                    </div>
-                    <div className="flex border rounded-xl relative">
-                        <input 
-                        className="w-full py-4 px-6 border rounded-xl bg-slate-50 focus:outline-none border-none"
-                        name="password"
-                        placeholder="password"
-                        type={`${show ? 'Hide' : 'Show'}`}
-                        />
-                    </div>
-                    <div className="flex border rounded-xl relative">
-                        <input 
-                        className="w-full py-4 px-6 border rounded-xl bg-slate-50 focus:outline-none border-none"
-                        type="password"
-                        name="confirm password"
-                        placeholder="Confirm Password"
-                        />
-                    </div> */}
                     <div className="input-button">
                         <button type="submit" className="w-full bg-gradient-to-r from-blue-500 to-indigo-500 rounded-md py-3 text-gray-50 text-lg
                         hover:bg-gradient-to-r hover:from-gray-50 hover:to-gray-100 hover:border-blue-500 hover:text-gray-700">
