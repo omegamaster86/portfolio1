d1cdfb69145eebc4f3f74b709519a80eb3f45168
omegamaster86 <masya868686@gmail.com>
2023-12-20 21:41:03 +0900

    add : callbacks

--- front/pages/api/auth/[...nextauth].js
+++ front/pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import axios from 'axios'
 
 export default NextAuth ({
   // Configure one or more authentication providers
@@ -9,4 +10,28 @@ export default NextAuth ({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account }) {
+      const provider = account.provider
+      const name = user.name
+      const email = user.email
+
+      try {
+        const response = await axios.post(`${apiUrl}/auth/${provider}/callback`, {
+          provider,
+          name,
+          email,
+        })
+
+        if (response.status === 200) {
+          return true
+        } else {
+          return false
+        }
+      } catch (error) {
+        console.log('エラー', error)
+        return false
+      }
+    },
+  },
 })
