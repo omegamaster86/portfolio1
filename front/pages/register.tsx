9ef6e8836692c1d8fb1e4a2a4292fefb1092b9df
omegamaster86 <masya868686@gmail.com>
2023-12-21 18:21:42 +0900

    ボタン名の変更

--- front/pages/register.tsx
+++ front/pages/register.tsx
@@ -21,7 +21,7 @@ export default function Register(){
                     <div className="input-button">
                         <button type="submit" className="w-full bg-gradient-to-r from-blue-500 to-indigo-500 rounded-md py-3 text-gray-50 text-lg
                         hover:bg-gradient-to-r hover:from-gray-50 hover:to-gray-100 hover:border-blue-500 hover:text-gray-700">
-                            Login
+                            Register
                         </button>
                     </div>
                 </form>
