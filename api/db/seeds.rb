b34113ac6ace8b3bb6b793bf4121d8114f3bf302
omegamaster86 <masya868686@gmail.com>
2023-12-23 17:05:26 +0900

    User.createを追加

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -9,3 +9,4 @@
 Todo.create(title: 'Todo 1', content: 'Todo 1 の内容')
 Todo.create(title: 'Todo 2', content: 'Todo 2 の内容')
 Todo.create(title: 'Todo 3', content: 'Todo 3 の内容')
+User.create(email: 'm.kanbara@repoc.jp', password: 'omega')
