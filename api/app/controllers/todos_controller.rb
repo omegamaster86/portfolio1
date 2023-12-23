ea873bea11e5a67ccbb5b30d49015a7f9a00510d
omegamaster86 <masya868686@gmail.com>
2023-12-23 18:56:46 +0900

    add : show

--- api/app/controllers/todos_controller.rb
+++ api/app/controllers/todos_controller.rb
@@ -4,4 +4,9 @@ class TodosController < ApplicationController
         render json: @todos
         # descは降順を指定している
     end
+    # GET /todos/:id
+    def show
+        @todo = Todo.find(params[:id])
+        render json: @todo
+    end
 end
