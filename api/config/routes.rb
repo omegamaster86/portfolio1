d437b88e79adde534f181e1717a7bac95fe746de
omegamaster86 <masya868686@gmail.com>
2023-12-23 14:14:34 +0900

    resources :todos, except: [:new, :edit]を追加

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :registers
   resources :users
   post 'auth/:provider/callback', to: 'api/v1/users#create'
- delete 'users/:email', to: 'api/v1/users#destroy', constraints: { email: %r{[^/]+} }
+  delete 'users/:email', to: 'api/v1/users#destroy', constraints: { email: %r{[^/]+} }
+  resources :todos, except: [:new, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
