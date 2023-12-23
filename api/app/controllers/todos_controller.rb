6c0f3ff510217edb4d74e951a08f750dc0cd5178
omegamaster86 <masya868686@gmail.com>
2023-12-23 13:54:23 +0900

    コメントを追加

--- api/app/controllers/todos_controller.rb
+++ api/app/controllers/todos_controller.rb
@@ -1,7 +1,7 @@
 class TodosController < ApplicationController
     def index
         @todos = Todo.all.order(created_at: :desc).limit(10)
-
         render json: @todos
+        # descは降順を指定している
     end
 end
