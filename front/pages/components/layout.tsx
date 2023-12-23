d303e911f57e06c2c3cdbbee5acc4ba5ab5d9f47
omegamaster86 <masya868686@gmail.com>
2023-12-23 13:02:44 +0900

    左側を空白にした

--- front/pages/components/layout.tsx
+++ front/pages/components/layout.tsx
@@ -10,7 +10,7 @@ export const Layout: React.FC<Props> = ({ children }) => {
         <div className="flex h-screen bg-blue-400">
             <div className="m-auto bg-slate-50 rounded-md w-3/5 grid lg:grid-cols-2">
                 <div className="bg-green-400">
-                    image
+                    
                 </div>
                 <div className="right flex flex-col justify-evenly">
                     <div className="text-center py-10">
