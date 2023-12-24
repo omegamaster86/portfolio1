2d02d7bdbe7f9ec8a4c87324adab39fa488d14d7
omegamaster86 <masya868686@gmail.com>
2023-12-25 00:51:40 +0900

    validates 追加

--- api/app/models/register.rb
+++ api/app/models/register.rb
@@ -1,3 +1,7 @@
 class Register < ApplicationRecord
     has_secure_password
+
+    validates :username, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 3 }
 end
