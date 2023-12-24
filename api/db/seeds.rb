b661523723f27f1f9f2d66b93fbf192ab34e9b9b
omegamaster86 <masya868686@gmail.com>
2023-12-25 00:51:52 +0900

    confirmpassword 追加

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -9,4 +9,4 @@
 Todo.create(title: 'Todo 1', content: 'Todo 1 の内容')
 Todo.create(title: 'Todo 2', content: 'Todo 2 の内容')
 Todo.create(title: 'Todo 3', content: 'Todo 3 の内容')
-User.create(email: 'm.kanbara@repoc.jp', password: 'omega')
+Register.create(username: 'bara', email: 'm.kanbara@repoc.jp', password: 'omega', confirmpassword: 'omega')
