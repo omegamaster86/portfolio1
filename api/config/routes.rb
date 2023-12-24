b4fd22ec2bf2ae1d410ce2c939175f7278d09175
omegamaster86 <masya868686@gmail.com>
2023-12-25 00:54:10 +0900

    change : resource

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  resources :registers
-  resources :users
-  post 'auth/:provider/callback', to: 'api/v1/users#create'
-  delete 'users/:email', to: 'api/v1/users#destroy', constraints: { email: %r{[^/]+} }
+  resources :registers, only: [:index, :show, :create, :update, :destroy]
+  resources :users, only: [:index, :show, :create, :update, :destroy]
   resources :todos, except: [:new, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
