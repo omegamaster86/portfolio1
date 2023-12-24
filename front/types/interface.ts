3353bec2e3b3bb87b33cd8fdd0645366a2f4607d
omegamaster86 <masya868686@gmail.com>
2023-12-24 21:07:16 +0900

    add : FormErrors,FormValues

---
+++ front/types/interface.ts
@@ -0,0 +1,14 @@
+export interface FormErrors {
+    username?: string;
+    email?: string;
+    password?: string;
+    confirmpassword?: string;
+    server?: string;
+  }
+  
+export interface FormValues {
+    username: string;
+    email: string;
+    password: string;
+    confirmpassword: string;
+  }
