69303495e38b66351315d7ed31d7f846d2999692
omegamaster86 <masya868686@gmail.com>
2023-12-25 00:51:00 +0900

    add : create

--- api/app/controllers/registers_controller.rb
+++ api/app/controllers/registers_controller.rb
@@ -1,51 +1,22 @@
 class RegistersController < ApplicationController
-  before_action :set_register, only: [:show, :update, :destroy]
 
-  # GET /registers
   def index
     @registers = Register.all
-
     render json: @registers
   end
 
-  # GET /registers/1
-  def show
-    render json: @register
-  end
-
-  # POST /registers
   def create
-    @register = Register.new(register_params)
-
-    if @register.save
-      render json: @register, status: :created, location: @register
+    @registers = Register.new(registers_params)
+    if @registers.save
+      render json: @registers, status: :created, location: @registers
     else
-      render json: @register.errors, status: :unprocessable_entity
+      render json: @registers.errors, status: :unprocessable_entity
     end
-  end
-
-  # PATCH/PUT /registers/1
-  def update
-    if @register.update(register_params)
-      render json: @register
-    else
-      render json: @register.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /registers/1
-  def destroy
-    @register.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_register
-      @register = Register.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def register_params
-      params.require(:register).permit(:name, :email, :password_digest, :confirmpassword)
-    end
+  def registers_params
+    params.require(:user).permit(:username, :email, :password, :confirmpassword)
+  end
 end
