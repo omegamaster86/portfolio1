ee34a206b59bc49116e06fa82c233cd98514a00d
omegamaster86 <masya868686@gmail.com>
2023-12-25 22:36:34 +0900

    バリデーションの修正

--- api/app/models/register.rb
+++ api/app/models/register.rb
@@ -1,7 +1,17 @@
+# class Register < ApplicationRecord
+#     has_secure_password
+
+#     validates :username, presence: true
+#     validates :email, presence: true, uniqueness: { case_sensitive: true }
+#     validates :password, presence: true, length: { minimum: 3 }
+# end
+
 class Register < ApplicationRecord
     has_secure_password
-
+  
     validates :username, presence: true
-    validates :email, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: { case_sensitive: true }
     validates :password, presence: true, length: { minimum: 3 }
-end
+    # password_confirmationのバリデーションはhas_secure_passwordによって自動的に処理される
+  end
+  
