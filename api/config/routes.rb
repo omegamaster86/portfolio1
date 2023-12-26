194c1e109678ac20549ca3ddbef13d6d719ee339
omegamaster86 <masya868686@gmail.com>
2023-12-26 19:38:29 +0900

    first commit

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :registers, only: [:index, :show, :create, :update, :destroy]
   resources :users, only: [:index, :show, :create, :update, :destroy]
+  get 'user', to: 'users#show'
   resources :todos, except: [:new, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
