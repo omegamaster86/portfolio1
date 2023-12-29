086037367360ce232d9bcab77e32e4fb36c83bac
omegamaster86 <masya868686@gmail.com>
2023-12-29 16:38:44 +0900

    Rails APIを叩いてユーザー情報をDBに保存する用のメソッドの追加

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -45,19 +45,29 @@ class UsersController < ApplicationController
 #     render json: { error: e.message }, status: :internal_server_error
 #   end
 
-  # PATCH/PUT /users/1
-#   def update
-#     if @user.update(user_params)
-#       render json: @user
-#     else
-#       render json: @user.errors, status: :unprocessable_entity
-#     end
-#   end
+  # Rails APIを叩いてユーザー情報をDBに保存する用のメソッド
+  def create
+    # 引数の条件に該当するデータがあればそれを返す。なければ新規作成する
+    user = User.find_or_create_by(provider: params[:provider], uid: params[:uid], name: params[:name], email: params[:email])                      
+    if user
+      head :ok
+    else
+      render json: { error: "ログインに失敗しました" }, status: :unprocessable_entity
+    end
+  rescue StandardError => e
+    render json: { error: e.message }, status: :internal_server_error
+  end
 
-  # DELETE /users/1
-#   def destroy
-#     @user.destroy
-#   end
+  def destroy
+    user = User.find_by(email: params[:email])
+    if user
+      user.destroy
+    else
+      render json: { error: "ユーザーが見つかりませんでした" }, status: :not_found
+    end
+  rescue StandardError => e
+    render json: { error: e.message }, status: :internal_server_error
+  end
 
   private
 
