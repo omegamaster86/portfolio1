9bd827937bac8b21d042a9636cf18db99be0defb
omegamaster86 <masya868686@gmail.com>
2023-12-29 16:35:28 +0900

    6、7行目のルート変更

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'user', to: 'users#show'
   post '/login', to: 'authentication#login'
   resources :todos, except: [:new, :edit]
+  post 'auth/:provider/callback', to: 'users#create'
+  delete 'users/:email', to: 'users#destroy', constraints: { email: %r{[^/]+} }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
