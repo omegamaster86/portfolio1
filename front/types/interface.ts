35432c0efa2a081214c96c52552178db9338d30f
omegamaster86 <masya868686@gmail.com>
2023-12-30 09:19:59 +0900

    first commit

---
+++ front/types/interface.ts
@@ -0,0 +1,14 @@
+export interface FormErrors {
+    username?: string;
+    email?: string;
+    password?: string;
+    password_confirmation?: string;
+    server?: string;
+  }
+  
+export interface FormValues {
+    username: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
+  }
