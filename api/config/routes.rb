5a7c882e78ad229e33fa44183f43fae5806d9bb3
omegamaster86 <masya868686@gmail.com>
2023-12-30 09:18:41 +0900

    first commit

--- api/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :users, only: [:index, :show, :create, :update, :destroy]
-  get 'user', to: 'users#show'
-  post '/login', to: 'authentication#login'
-  resources :todos, except: [:new, :edit]
-  post 'auth/:provider/callback', to: 'users#create'
-  delete 'users/:email', to: 'users#destroy', constraints: { email: %r{[^/]+} }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
